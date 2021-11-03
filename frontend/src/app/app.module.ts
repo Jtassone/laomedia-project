f7ed105afb9b0e12effe1ced4ba651418f0c2bba
Renee St Louis <rstlouis@wpi.edu>
2021-11-03 17:15:55 -0400

    Laying some groundwork

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClassificationsComponent } from './classifications/classifications.component';
+import { AlgorithmComponent } from './algorithm/algorithm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassificationsComponent,
+    AlgorithmComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/classifications/classifications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lao-classifications',
+  templateUrl: './classifications.component.html',
+  styleUrls: ['./classifications.component.scss']
+})
+export class ClassificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

6671142a1d22d3eace76090c3e4e7f8069dc7473
Joshua Tassone <jtassone93@gmail.com>
2021-11-07 14:27:02 -0500

    create the options lambda for preflight requests

---
+++ src/main/java/com/aws/codestar/projecttemplates/handler/OptionsHandler.java
@@ -0,0 +1,21 @@
+package com.aws.codestar.projecttemplates.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.aws.codestar.projecttemplates.GatewayResponse;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OptionsHandler implements RequestHandler<Object, Object> {
+
+    public Object handleRequest(final Object input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Access-Control-Allow-Origin", "*");
+        headers.put("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS");
+        headers.put("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        headers.put("Content-Type", "application/json");
+        return new GatewayResponse(new JSONObject().put("Output", "Hello World! Laomedia").toString(), headers, 200);
+    }
+}

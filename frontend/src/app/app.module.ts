8caf267ef88fac03d3f803e492eb0eb884f248e5
Vinit <vinitkothari28@gmail.com>
2021-11-08 21:58:21 -0500

    Amplify stuff

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import Amplify from 'aws-amplify';
+
+import {awsconfig} from 'backend-aws-exports/dev/aws-exports';
+
+
+
+
+/* Configure Amplify resources */
+Amplify.configure(awsconfig);
+
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
+
   declarations: [
     AppComponent
   ],

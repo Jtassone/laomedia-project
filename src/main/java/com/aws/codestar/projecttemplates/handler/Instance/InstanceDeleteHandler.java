f0780d6268482c6db48f7731c5e001e29e6b539c
Conor McDonough <cemcdonough@wpi.edu>
2021-12-11 15:31:58 -0500

    Added get and delete instances

--- src/main/java/com/aws/codestar/projecttemplates/handler/Instance/InstanceDeleteHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/Instance/InstanceDeleteHandler.java
@@ -1,31 +1,31 @@
 package com.aws.codestar.projecttemplates.handler.Instance;
 
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.aws.codestar.projecttemplates.handler.Algorithm.Algorithm;
-import com.aws.codestar.projecttemplates.handler.Algorithm.AlgorithmService;
-import com.aws.codestar.projecttemplates.handler.Classification.Classification;
 import com.aws.codestar.projecttemplates.handler.Classification.ClassificationService;
 import com.aws.codestar.projecttemplates.utils.RDSClient;
 import com.google.gson.Gson;
-import org.json.JSONObject;
 
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
 
-public class InstanceHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class InstanceDeleteHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     Connection sqlConnection;
     Gson gson;
-
-    public InstanceHandler() {
+    //
+    public InstanceDeleteHandler() {
         this.sqlConnection = RDSClient.getRemoteConnection();
         this.gson = new Gson();
     }
+
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
@@ -33,14 +33,17 @@ public class InstanceHandler implements RequestHandler<APIGatewayProxyRequestEve
         HashMap<String, String> headers = new HashMap<String, String>();
         headers.put("Content-Type", "application/json");
         try {
-            List<Instance> instancesList = InstanceService.getAllInstances(sqlConnection);
-            response.setBody(gson.toJson(instancesList));
+            String id = event.getPathParameters().get("proxy").split("/")[1];
+            InstanceService.deleteInstance(sqlConnection, id);
+            response.setBody(gson.toJson(id));
             response.setStatusCode(200);
-        } catch (SQLException e) {
+        } catch (Exception e) {
             System.out.println(e);
             response.setStatusCode(500);
         }
         System.out.println("The response is" + response);
         return response;
     }
+
+
 }
--- src/main/java/com/aws/codestar/projecttemplates/handler/Instance/InstanceService.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/Instance/InstanceService.java
@@ -1,10 +1,17 @@
 package com.aws.codestar.projecttemplates.handler.Instance;
 
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.aws.codestar.projecttemplates.handler.Classification.Classification;
 import com.aws.codestar.projecttemplates.utils.S3Client;
+import com.aws.codestar.projecttemplates.utils.UUIDUtil;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class InstanceService {
 
@@ -20,6 +27,37 @@ public class InstanceService {
         } catch (SQLException e) {
             System.out.println(e);
             throw e;
+        }
+    }
+
+    public static List<Instance> getAllInstances(Connection sqlConnection) throws SQLException {
+        List<Instance> instanceList = new ArrayList<>();
+        try (ResultSet sqlResponse = sqlConnection.createStatement().executeQuery("select * from instance")) {
+            while (sqlResponse.next()) {
+                UUID id = UUIDUtil.getUUIDFromBytes(sqlResponse.getBytes("id"));;
+                String name = sqlResponse.getString("name");
+                byte[] parentAlgorithmId = sqlResponse.getBytes("parent_algorithm_id");
+                byte[] parentImplementationId = sqlResponse.getBytes("parent_implementation_id");
+                Instance instance = new Instance(id, name, UUIDUtil.getUUIDFromBytes(parentAlgorithmId),
+                        UUIDUtil.getUUIDFromBytes(parentImplementationId));
+                instanceList.add(instance);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw e;
+        }
+        return instanceList;
+    }
+
+    public static void deleteInstance(Connection sqlConnection, String id) throws Exception {
+        UUID class_uuid = UUID.fromString(id);
+        try {
+            String deleteSQL = "DELETE FROM instances WHERE id = uuid_to_bin(" + "\"" + class_uuid + "\"" + ")";
+            PreparedStatement preparedStatement = sqlConnection.prepareStatement(deleteSQL);
+            preparedStatement.executeUpdate();
+
+        } catch (Exception e) {
+            throw new Exception("Failed to delete Classification: " + e.getMessage());
         }
     }
 }

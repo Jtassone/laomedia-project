ae64596fcb046c09aa0024bdc852a9cbfa2668a7
Joshua Tassone <jtassone93@gmail.com>
2021-12-11 16:50:22 -0500

    allow all methods

--- src/main/java/com/aws/codestar/projecttemplates/handler/OptionsHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/OptionsHandler.java
@@ -13,7 +13,7 @@ public class OptionsHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Access-Control-Allow-Origin", "*");
-        headers.put("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        headers.put("Access-Control-Allow-Methods", "*");
         headers.put("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
         headers.put("Content-Type", "application/json");
         return new GatewayResponse(new JSONObject().put("Output", "Hello World! Laomedia").toString(), headers, 200);

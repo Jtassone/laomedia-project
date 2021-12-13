43ca1a4958ac0cb56955745d7dea2ebaba293815
Vinit <vinitkothari28@gmail.com>
2021-12-12 19:38:43 -0500

    Handler Tests to be added

---
+++ src/test/java/com/aws/codestar/projecttemplates/handler/HandlerTests.java
@@ -0,0 +1,34 @@
+package java.com.aws.codestar.projecttemplates.handler;
+import org.junit.*;
+
+import static junit.framework.TestCase.assertEquals;
+
+
+public class HandlerTests {
+
+    @Test
+    public void testAlgosDelete(){
+        System.out.println("hello");
+        assertEquals(false, false);
+
+    }
+
+
+    @Test public void testAlgosPost(){
+
+    }
+
+    @Test public void testAlgosGet(){
+
+    }
+
+    @Test public void testClassifictionsGet(){
+
+    }
+
+    @Test public void testPostClassifications(){
+        
+    }
+
+
+}

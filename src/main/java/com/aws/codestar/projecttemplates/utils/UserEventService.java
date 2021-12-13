be98821a69d93f27664c214701a22bc69fab15da
Joshua Tassone <jtassone93@gmail.com>
2021-12-13 00:30:21 -0500

    fix typo in user_events table

--- src/main/java/com/aws/codestar/projecttemplates/utils/UserEventService.java
+++ src/main/java/com/aws/codestar/projecttemplates/utils/UserEventService.java
@@ -11,7 +11,7 @@ public class UserEventService {
     public static void logUserEvent(Connection sqlConnection, UserEvent event) throws SQLException {
         byte[] userEventId = UUIDUtil.getBytesFromUUID(event.id);
         try {
-            PreparedStatement preparedStatement = sqlConnection.prepareStatement( "INSERT INTO user_Events (id, user_name, event_verb, event_details) VALUES (?, ? , ? , ?)");
+            PreparedStatement preparedStatement = sqlConnection.prepareStatement( "INSERT INTO user_events (id, user_name, event_verb, event_details) VALUES (?, ? , ? , ?)");
             preparedStatement.setBytes(1, userEventId);
             preparedStatement.setString(2, event.userName);
             preparedStatement.setString(3, event.eventVerb);

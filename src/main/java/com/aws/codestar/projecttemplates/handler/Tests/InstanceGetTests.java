96e72b2af91e4cd2f11a68f37d4d0120ece0fdac
Conor McDonough <cemcdonough@wpi.edu>
2021-12-15 22:42:04 -0500

    Instance Tests

---
+++ src/main/java/com/aws/codestar/projecttemplates/handler/Tests/InstanceGetTests.java
@@ -0,0 +1,55 @@
+package com.aws.codestar.projecttemplates.handler.Tests;
+
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.aws.codestar.projecttemplates.handler.Algorithm.Algorithm;
+import com.aws.codestar.projecttemplates.handler.Algorithm.AlgorithmDeleteHandler;
+import com.aws.codestar.projecttemplates.handler.Algorithm.AlgorithmHandler;
+import com.aws.codestar.projecttemplates.handler.Algorithm.AlgorithmPostHandler;
+import com.aws.codestar.projecttemplates.handler.Instance.InstanceHandler;
+import com.aws.codestar.projecttemplates.utils.RDSClient;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class InstanceGetTests {
+
+    private RDSClient mockRDSService;
+    private InstanceHandler instanceHandler;
+    private TestContext context;
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+
+    @Test
+    public void validRequest3() throws SQLException {
+        instanceHandler = new InstanceHandler();
+        context = new TestContext();
+        Gson gson = new Gson();
+
+        APIGatewayProxyRequestEvent getRequest = new APIGatewayProxyRequestEvent()
+                .withQueryStringParameters(Map.of("userName", "testUser"));
+
+        APIGatewayProxyResponseEvent getResponse = this.instanceHandler.handleRequest(
+                getRequest,
+                context
+        );
+
+
+        assertEquals(200, (int) getResponse.getStatusCode());
+    }
+
+}
---
+++ src/main/java/com/aws/codestar/projecttemplates/handler/Tests/InstancePostTests.java
@@ -0,0 +1,57 @@
+package com.aws.codestar.projecttemplates.handler.Tests;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.aws.codestar.projecttemplates.handler.Algorithm.Algorithm;
+import com.aws.codestar.projecttemplates.handler.Algorithm.AlgorithmDeleteHandler;
+import com.aws.codestar.projecttemplates.handler.Algorithm.AlgorithmHandler;
+import com.aws.codestar.projecttemplates.handler.Algorithm.AlgorithmPostHandler;
+import com.aws.codestar.projecttemplates.handler.Instance.Instance;
+import com.aws.codestar.projecttemplates.handler.Instance.InstancePostHandler;
+import com.aws.codestar.projecttemplates.utils.RDSClient;
+import com.google.gson.Gson;
+import org.testng.annotations.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.UUID;
+
+public class InstancePostTests {
+
+
+    private RDSClient mockRDSService;
+    private InstancePostHandler instancePostHandler;
+    private TestContext context;
+
+
+
+    @Test
+    public void validRequest() throws SQLException {
+        instancePostHandler = new InstancePostHandler();
+        context = new TestContext();
+        // 1. Arrange
+        Instance instance = new Instance(UUID.randomUUID(), "test instance 1", UUID.fromString("ee2eb871-36a8-484a-bba4-7c3ebd714bdb"), UUID.fromString("ee2eb871-36a8-484a-bba4-7c3ebd714bd2"));
+
+
+        // Fake AWS Lambda request from API Gateway
+        APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent()
+                .withBody("{\"name\":\"test instance 1\", \"algorithmId\": \"ee2eb871-36a8-484a-bba4-7c3ebd714bdb\", \"implementationId\": \"ee2eb871-36a8-484a-bba4-7c3ebd714bd2\"}")
+                .withQueryStringParameters(Map.of("userName", "testUser"));
+
+
+        // 2. Act
+        APIGatewayProxyResponseEvent response = this.instancePostHandler.handleRequest(
+                request,
+                context
+        );
+
+        Gson gson = new Gson();
+        // 3. Assert
+        Instance instanceResponse = gson.fromJson(response.getBody(), Instance.class);
+        assertEquals(200, (int) response.getStatusCode());
+        assertEquals(instance.name, instanceResponse.name);
+        assertEquals(instance.algorithmId, instanceResponse.algorithmId);
+    }
+
+}
